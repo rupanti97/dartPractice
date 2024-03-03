917c8a35500c479ac79c85c683636f01d3978e9b
rupanti97 <rupanti97nntec@gmail.com>
2024-03-03 14:30:32 +0600

    New files added

---
+++ bin/human.dart
@@ -0,0 +1,10 @@
+class Human{
+  int legs;
+  int hands;
+  Human(this.legs,this.hands);
+}
+main(){
+  Human humanOne = Human(2,2);
+  print("Human legs ${humanOne.legs}");
+  print("Human hands ${humanOne.hands}");
+}
